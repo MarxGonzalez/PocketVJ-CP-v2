eebbd189519183767abe6a1f21e655e0a175c3d0
magdesign <mag@magdesign.ch>
2015-12-13 20:02:19 +0100

    Fixed Play Video  02

--- sync/startmaster02.py
+++ sync/startmaster02.py
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/omxplayer-sync -mu -o both /media/internal/02* > /dev/null 2>&1 & echo $! &
+/usr/bin/omxplayer-sync -mu -o both /media/internal/video/02* > /dev/null 2>&1 & echo $! &
